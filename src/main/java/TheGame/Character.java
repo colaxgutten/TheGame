63b5162dadfa97c0afc4ebd407b092e8df4647ef
Eirik Strom <eirikstrom1@gmail.com>
2017-01-23 16:21:52 +0100

    Replaced walkLeft() etc with walk() and stop()

--- src/main/java/TheGame/Character.java
+++ src/main/java/TheGame/Character.java
@@ -6,9 +6,11 @@ import javafx.geometry.Point2D;
 
 public class Character extends Animatable {
 	protected Game game;
-	private enum Dir {Up, Down, Left, Right};
+	public enum Dir {Up, Down, Left, Right}
 	
 	private Dir walkDir;
+	private Dir nextDir;
+	private Point2D stopAt;
 	private double speed;
 	private long lastUpdate;
 	private Point2D truePos;
@@ -21,43 +23,48 @@ public class Character extends Animatable {
 	}
 	
 	public void walkLeft(long time) {
-		if(this.walkDir == null) {
-			this.setCurrentAnimation("walkLeft", time);
-			this.walkDir = Dir.Left;
-		}
 	}
 	
 	public void walkRight(long time) {
-		if(this.walkDir == null) {
-			this.setCurrentAnimation("walkRight", time);
-			this.walkDir = Dir.Right;
-		}
 	}
 	
 	public void walkUp(long time) {
-		if(this.walkDir == null) {
-			this.setCurrentAnimation("walkUp", time);
-			this.walkDir = Dir.Up;
-		}
 	}
 	
-	public void walkDown(long time) {
+	public void walkDown(long time) {/*
 		if(this.walkDir == null) {
 			this.setCurrentAnimation("walkDown", time);
 			this.walkDir = Dir.Down;
-		}
+		}*/
 	}
 	
 	public Point2D getPos() {
 		return truePos;
 	}
-	
+
+	public void walk(Dir dir) {
+		if(walkDir != null) {
+			stopAt = truePos;
+			nextDir = dir;
+		} else {
+			walkDir = dir;
+		}
+	}
+
+	public void stop() {
+		stopAt = truePos;
+	}
+
+	public void step(Dir dir, int steps) {
+
+	}
+
 	public void update(long time) {
 		if(walkDir == null) {
 			lastUpdate = time;
 			return;
 		}
-		
+
 		double dist;
 		if(lastUpdate == 0)
 			dist = 0;
@@ -65,45 +72,58 @@ public class Character extends Animatable {
 			dist = speed * (time - lastUpdate);
 
 		switch(walkDir) {
-		case Left:
-			truePos = truePos.subtract(dist, 0);
-			break;
-		case Right:
-			truePos = truePos.add(dist, 0);
-			break;
-		case Up:
-			truePos = truePos.subtract(0, dist);
-			break;
-		case Down:
-			truePos = truePos.add(0, dist);
-			break;
+			case Left:
+				truePos = truePos.subtract(dist, 0);
+				break;
+			case Right:
+				truePos = truePos.add(dist, 0);
+				break;
+			case Up:
+				truePos = truePos.subtract(0, dist);
+				break;
+			case Down:
+				truePos = truePos.add(0, dist);
+				break;
 		}
 
-		if(truePos.getX() - getX() >= 1) {
-			setPos(getX() + 1, getY());
-			truePos = new Point2D(getX(), getY());
-			walkDir = null;
-			this.setCurrentAnimation("lookRight", time);
+		if(stopAt != null) {
+			if(truePos.getX() > Math.ceil(stopAt.getX())) {
+				setPos((int)Math.ceil(stopAt.getX()), getY());
+				truePos = new Point2D(getX(), getY());
+				walkDir = nextDir;
+				nextDir = null;
+				stopAt = null;
+				this.setCurrentAnimation("lookRight", time);
+			}
+
+			else if(truePos.getX() < Math.floor(stopAt.getX())) {
+				setPos((int)Math.floor(stopAt.getX()), getY());
+				truePos = new Point2D(getX(), getY());
+				walkDir = nextDir;
+				nextDir = null;
+				stopAt = null;
+				this.setCurrentAnimation("lookLeft", time);
+			}
+
+			else if(truePos.getY() > Math.ceil(stopAt.getY())) {
+				setPos(getX(), (int)Math.ceil(stopAt.getY()));
+				truePos = new Point2D(getX(), getY());
+				walkDir = nextDir;
+				nextDir = null;
+				stopAt = null;
+				this.setCurrentAnimation("lookDown", time);
+			}
+
+			else if(truePos.getY() < Math.floor(stopAt.getY())) {
+				setPos(getX(), (int)Math.floor(stopAt.getY()));
+				truePos = new Point2D(getX(), getY());
+				walkDir = nextDir;
+				nextDir = null;
+				stopAt = null;
+				this.setCurrentAnimation("lookUp", time);
+			}
 		}
-		if(truePos.getX() - getX() <= -1) {
-			setPos(getX() - 1, getY());
-			truePos = new Point2D(getX(), getY());
-			walkDir = null;
-			this.setCurrentAnimation("lookLeft", time);
-		}
-		if(truePos.getY() - getY() >= 1) {
-			setPos(getX(), getY() + 1);
-			truePos = new Point2D(getX(), getY());
-			walkDir = null;
-			this.setCurrentAnimation("lookDown", time);
-		}
-		if(truePos.getY() - getY() <= -1) {
-			setPos(getX(), getY() - 1);
-			truePos = new Point2D(getX(), getY());
-			walkDir = null;
-			this.setCurrentAnimation("lookUp", time);
-		}
-		
+
 		lastUpdate = time;
 	}
 }
